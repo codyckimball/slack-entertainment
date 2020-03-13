fbd6307d85f12839cdb2c186634f26157d38ce9b
kevinmouritsen <kgmour@gmail.com>
2020-03-13 11:22:51 -0600

    adding resources directory with sql schema file

---
+++ resources/schema.sql
@@ -0,0 +1,28 @@
+CREATE TABLE public.user
+( user_id SERIAL PRIMARY KEY,
+ slack_handle TEXT NOT NULL UNIQUE,
+ slack_name TEXT NOT NULL,
+ created_on TIMESTAMP NOT NULL)
+
+CREATE TABLE IF NOT EXISTS show
+( imdb_id TEXT PRIMARY KEY,
+ title TEXT NOT NULL,
+ year INTEGER NOT NULL,
+ release_date DATE,
+ runtime INTEGER,
+ genre TEXT,
+ director TEXT,
+ written_by TEXT,
+ synopsis TEXT,
+ actors TEXT,
+ awards TEXT,
+ imdb_rating TEXT,
+ rotten_tomatoes_rating TEXT,
+ total_seasons TEXT )
+
+CREATE TABLE IF NOT EXISTS rating
+( rating_id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES public.user(user_id) NOT NULL,
+  imdb_id TEXT NOT NULL,
+  rating INTEGER NOT NULL,
+  comment TEXT )
