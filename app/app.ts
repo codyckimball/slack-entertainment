449ea8db99dff3f1ca3ab9e3bbd17c1e83554c08
kevinmouritsen <kgmour@gmail.com>
2020-03-27 10:35:33 -0600

    removing node_modules, additional queries in the sql schema, additional tsconfig files

---
+++ app/app.ts
@@ -0,0 +1,3 @@
+function startGame() {
+    var messagesElement = document.getElementById
+}
--- resources/schema.sql
+++ resources/schema.sql
@@ -1,6 +1,7 @@
 CREATE TABLE public.user
 ( user_id SERIAL PRIMARY KEY,
- slack_handle TEXT NOT NULL UNIQUE,
+ slack_id TEXT NOT NULL UNIQUE,
+ slack_handle TEXT NOT NULL,
  slack_name TEXT NOT NULL,
  created_on TIMESTAMP NOT NULL)
 
@@ -27,3 +28,29 @@ CREATE TABLE IF NOT EXISTS rating
   imdb_id TEXT NOT NULL,
   rating INTEGER NOT NULL,
   comment TEXT )
+
+  #INSERT INTO rating 
+  #(user_id, imdb_id, rating, comment) VALUES ()
+
+  SELECT AVG(rating) from rating 
+  JOIN show
+  ON rating.imdb_id = show.imdb_id
+  WHERE show.title = "<some value>";
+
+  SELECT s.title, r.rating, r.comment
+  FROM show as s
+  JOIN rating as r
+  ON s.imdb_id = r.imdb_id
+  JOIN user as u 
+  ON r.user_id = u.user_id
+  WHERE u.slack_id = "<some value>";
+
+  SELECT s.title, AVG(r.rating) as avg from rating as r  
+  JOIN show as s 
+  ON rating.imdb_id = show.imdb_id
+  WHERE s.show_type = "<some value>"
+  GROUP BY s.title
+  ORDER BY avg DESC limit 10;
+
+  SELECT * from show WHERE title = "<some value>";
+
